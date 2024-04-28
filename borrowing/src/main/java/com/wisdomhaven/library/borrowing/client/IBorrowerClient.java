f8b8ce33c95afa9b33ffcb12db96ae33b937fc8a
JHSW <50164113+jhchinGit@users.noreply.github.com>
2024-04-28 13:35:39 +0800

    Optimize mapping

--- borrowing/src/main/java/com/wisdomhaven/library/borrowing/client/IBorrowerClient.java
+++ borrowing/src/main/java/com/wisdomhaven/library/borrowing/client/IBorrowerClient.java
@@ -3,12 +3,11 @@ package com.wisdomhaven.library.borrowing.client;
 import com.wisdomhaven.library.borrowing.dto.apiResult.Borrower;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name="borrowerClient", url = "localhost:8091/borrowers")
 public interface IBorrowerClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/{borrowerId}", produces = "application/json")
+    @GetMapping(value = "/{borrowerId}", produces = "application/json")
     ResponseEntity<Borrower> getBorrower(@PathVariable("borrowerId") Integer borrowerId);
 }
